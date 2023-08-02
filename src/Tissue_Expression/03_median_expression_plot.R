01f8f54fd97524e185ea29c2f820a0dff52e3400
Elizabeth J Ramsey <lizzyr@c0167.cm.cluster>
2023-08-02 09:19:56 +0000

    separate violin plots for setbp1 vs known targets

--- src/Tissue_Expression/03_median_expression_plot.R
+++ src/Tissue_Expression/03_median_expression_plot.R
@@ -22,9 +22,74 @@ setbp1_files <- list.files(
 #pull SETBP1 expression across samples
 #ERROR in `dplyr::bind_cols()`:! Can't recycle `..1` (size 1293) to match `..2` (size 274).
 #setbp1_exp <- map_dfc(setbp1_files, pull_gene_gtex)
+#setbp1_exp <- map_dfc(setbp1_files, pull_gene_gtex)
+#BiocManager::install("rowr")
+#library(rowr)
+t <- pull_gene_gtex(setbp1_files[1])
+setbp1_exp_list <- map(setbp1_files, pull_gene_gtex)
+#t2 <- do.call(cbind.data.frame, t)
+setbp1_exp <- map_dfr(setbp1_files, pull_gene_gtex)
+
+setbp1_med_tpm_vp <- setbp1_exp %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = "Tissue", values_to = "TPM"
+  ) %>%
+  mutate(
+    TPM = log(1 + TPM, base = 2),
+    .keep = "unused"
+  ) %>% # scaling
+  # mutate(
+  #   Tissue = str_extract(
+  #     Tissue,
+  #     ".*(?=TPM)"
+  #   ),
+  #   .keep = "unused"
+  # ) %>% # remove excess from filenames
+  mutate(Tissue = str_replace_all(Tissue, " ", "")) %>% # remove whitespace
+  mutate(Affected = ifelse(Tissue %in% affected, "TRUE", "FALSE")) %>%
+  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " "))) #%>%
+
+#plot
+p <- setbp1_med_tpm_vp %>%  ggplot(aes(
+  x = reorder(Tissue, TPM, FUN = median),
+  y = TPM, color = Affected
+)) +
+  geom_violin(trim = FALSE) +
+  scale_color_manual(values = c(
+    "TRUE" = alpha("#2C1D6C", 0.75),
+    "FALSE" = alpha("#D3B1C5", 0.5)
+  )) +
+  ylab("TPM Scaled (1+log2)") +
+  ggtitle("Expression of SETBP1 Across GTEx Tissues") +
+  labs(x = "GTEx Tissue") +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    text = element_text(family = "Helvetica"),
+    axis.text.x = element_text(color = "black", size = 10),
+    axis.text.y = element_text(color = "black"),
+    axis.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"),
+    legend.text = element_text(face = "bold"),
+    axis.title.y = element_text(face = "bold"),
+    axis.title.x = element_text(face = "bold"),
+    title = element_text(face = "bold"),
+    plot.title = element_text(size = 14, hjust = 0.5)
+  )
+# add boxplot
+p <- p + geom_boxplot(width = 0.1) + coord_flip()
+ggsave(
+  here(
+    "results/SETBP1_Expression/plots/setbp1_tpm_scaled_violin.png"
+  ),
+  height = 8, width = 6
+)
+
 
 # calculate median tpm of each gene for each tissue and compile list
-tissues_median_tpm <- map_dfc(setbp1_files[6:31], get_gtex_median_tpm)
+tissues_median_tpm <- map_dfc(setbp1_files, get_gtex_median_tpm)
 
 # remove any columns with NAs
 tissues_median_tpm <- tissues_median_tpm %>% select_if(~ !any(is.na(.)))
@@ -46,9 +111,10 @@ affected <- c(
   "STOMACH",
   "ESOPHAGUS"
 )
-
+#ENSG00000152217
 ## VIOLIN PLOT
-p <- tissues_median_tpm %>%
+tissues_median_tpm <- tissues_median_tpm[!(row.names(tissues_median_tpm) %in% "ENSG00000152217"),]  #remove setbp1
+tissues_med_tpm_vp <- tissues_median_tpm %>%
   pivot_longer(
     cols = everything(),
     names_to = "Tissue", values_to = "Median_TPM"
@@ -66,8 +132,9 @@ p <- tissues_median_tpm %>%
   ) %>% # remove excess from filenames
   mutate(Tissue = str_replace_all(Tissue, " ", "")) %>% # remove whitespace
   mutate(Affected = ifelse(Tissue %in% affected, "TRUE", "FALSE")) %>%
-  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " "))) %>%
-  ggplot(aes(
+  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " "))) #%>%
+# plot
+p <- tissues_med_tpm_vp %>%  ggplot(aes(
     x = reorder(Tissue, Median_TPM, FUN = median),
     y = Median_TPM, color = Affected
   )) +
@@ -77,7 +144,7 @@ p <- tissues_median_tpm %>%
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
   ylab("Median TPM Scaled (1+log2)") +
-  ggtitle("Expression of SETBP1 and Targets Across GTEx Tissues") +
+  ggtitle("Expression of SETBP1 Targets Across GTEx Tissues") +
   labs(x = "GTEx Tissue") +
   theme(
     panel.grid.major = element_blank(),
@@ -98,7 +165,7 @@ p <- tissues_median_tpm %>%
 p <- p + geom_boxplot(width = 0.1) + coord_flip()
 ggsave(
   here(
-    "results/SETBP1_Expression/plots/median_tpm_scaled_violin_flip.png"
+    "results/SETBP1_Expression/plots/setbp1targets_median_tpm_scaled_violin.png"
   ),
   height = 8, width = 6
 )
