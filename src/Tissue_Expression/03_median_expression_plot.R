30aef279c11a4d7539d0e6c55fc3cb2d92af9689
Elizabeth J Ramsey <lizzyr@c0177.cm.cluster>
2023-08-04 19:12:18 +0000

    cleaned up code and linted

--- src/Tissue_Expression/03_median_expression_plot.R
+++ src/Tissue_Expression/03_median_expression_plot.R
@@ -67,14 +67,14 @@ p <- setbp1_med_tpm_vp %>% ggplot(aes(
     "TRUE" = alpha("#2C1D6C", 0.75),
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
-  # geom_text(aes(y = Max, label = nSamples, vjust = 0.5, hjust = -1, fontface = "bold")) +
-  geom_text(aes(y = Max, label = nSamples,
-                vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
+  geom_text(aes(
+    y = Max, label = nSamples,
+    vjust = 0.5, fontface = "bold"
+  ), nudge_y = 1.2) +
   scale_y_continuous(limits = c(-0.7, 7)) +
   ylab("TPM Scaled (1+log2)") +
   labs(x = "GTEx Tissue") +
   theme(
-    # plot.margin = margin(0.1, 2.5, 0.1, 0.1, "cm"), #top, right, bottom, left
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
@@ -99,7 +99,9 @@ ggsave(
 
 
 # RIDGELINE PLOT - SETBP1 TISSUES
-sb1_tissue_plot <- ggplot(setbp1_med_tpm_vp, aes(x = TPM, y = reorder(Tissue, TPM, FUN = median), fill = Affected)) +
+sb1_tissue_plot <- ggplot(setbp1_med_tpm_vp,
+                          aes(x = TPM, y = reorder(Tissue, TPM, FUN = median),
+                              fill = Affected)) +
   ggridges:::stat_density_ridges(
     quantile_lines = TRUE, quantiles = 2,
     alpha = 0.8
@@ -108,9 +110,9 @@ sb1_tissue_plot <- ggplot(setbp1_med_tpm_vp, aes(x = TPM, y = reorder(Tissue, TP
     "TRUE" = alpha("#2C1D6C", 0.75),
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
-  geom_text(aes(x = Max, label = nSamples, fontface = "bold"), nudge_x = 1, nudge_y = 0.22) +
-  # scale_y_continuous(limits = c(-0.7, 7)) +
-  # geom_text(aes(y = nSamples, label = Max, vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
+  geom_text(
+    aes(x = Max, label = nSamples, fontface = "bold"),
+    nudge_x = 1, nudge_y = 0.22) +
   scale_x_continuous(limits = c(-0.5, 7)) +
   labs(x = "TPM Scaled (1+log2)", y = "GTEx Tissue") +
   theme(
@@ -138,13 +140,17 @@ ggsave(
 
 ### SETBP1 EXPRESSION BY BRAIN REGION
 # gtex metadata for brain regions
-gtex_meta <- read.delim(here("data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt"))
+gtex_meta <- read.delim(
+  here("data/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt"))
 brain_meta <- filter(gtex_meta, SMTS == "Brain")
-brain_tpm <- read.csv(here("results/SETBP1_Expression/BRAIN_SETBP1_targets_tpm.csv"), row.names = 1)
+brain_tpm <- read.csv(
+  here("results/SETBP1_Expression/BRAIN_SETBP1_targets_tpm.csv"), row.names = 1)
 
 # find brain regions
 brain_regions <- brain_tpm %>%
-  pivot_longer(., cols = starts_with("GTEX"), names_to = "SAMPID", values_to = "TPM") %>%
+  pivot_longer(.,
+               cols = starts_with("GTEX"),
+               names_to = "SAMPID", values_to = "TPM") %>%
   mutate(SAMPID = str_replace(SAMPID, "\\.1$", "")) %>% # remove trailing .1's
   mutate(SAMPID = str_replace_all(SAMPID, "\\.", "-")) %>% # sub . for _
   inner_join(., brain_meta, by = "SAMPID") %>% # combine with gtex metadata
@@ -155,7 +161,9 @@ brain_regions <- brain_tpm %>%
 br_plot <- brain_regions %>%
   mutate(TPM = log(1 + TPM, base = 2)) %>%
   mutate(Region = str_trunc(Region, 20)) %>%
-  ggplot(., aes(x = TPM, y = reorder(Region, TPM, FUN = median), fill = Tissue)) +
+  ggplot(.,
+         aes(x = TPM, y = reorder(Region, TPM, FUN = median),
+             fill = Tissue)) +
   ggridges:::stat_density_ridges(quantile_lines = TRUE, quantiles = 2) +
   scale_fill_manual(values = c(alpha("#2C1D6C", 0.65))) +
   labs(x = "TPM Scaled (1+log2)", y = "") +
