b11e7e34b2820c54fb1b126cbd9b7af540bce8a9
Elizabeth J Ramsey <lizzyr@c0177.cm.cluster>
2023-08-04 19:06:33 +0000

    updated font

--- src/Tissue_Expression/03_median_expression_plot.R
+++ src/Tissue_Expression/03_median_expression_plot.R
@@ -52,29 +52,29 @@ setbp1_med_tpm_vp <- setbp1_exp %>%
   ) %>% # scaling
   mutate(Tissue = str_replace_all(Tissue, " ", "")) %>% # remove whitespace
   mutate(Affected = ifelse(Tissue %in% affected, "TRUE", "FALSE")) %>%
-  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " ")))  %>%
+  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " "))) %>%
   group_by(Tissue) %>%
   mutate(nSamples = sum(!is.na(TPM))) %>%
-  mutate(Max = max(TPM, na.rm = TRUE)) #%>%
-  #mutate(Median = median(TPM, na.rm = TRUE))
-  
-  # VIOLIN PLOT - SETBP1 TISSUES
-  p <- setbp1_med_tpm_vp %>% ggplot(aes(
-    x = reorder(Tissue, TPM, FUN = median),
-    y = TPM, color = Affected
-  )) +
+  mutate(Max = max(TPM, na.rm = TRUE)) # %>%
+
+# VIOLIN PLOT - SETBP1 TISSUES
+p <- setbp1_med_tpm_vp %>% ggplot(aes(
+  x = reorder(Tissue, TPM, FUN = median),
+  y = TPM, color = Affected
+)) +
   geom_violin(trim = FALSE) +
   scale_color_manual(values = c(
     "TRUE" = alpha("#2C1D6C", 0.75),
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
-  #geom_text(aes(y = Max, label = nSamples, vjust = 0.5, hjust = -1, fontface = "bold")) +
-  geom_text(aes(y = Max, label = nSamples, vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
+  # geom_text(aes(y = Max, label = nSamples, vjust = 0.5, hjust = -1, fontface = "bold")) +
+  geom_text(aes(y = Max, label = nSamples,
+                vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
   scale_y_continuous(limits = c(-0.7, 7)) +
   ylab("TPM Scaled (1+log2)") +
   labs(x = "GTEx Tissue") +
   theme(
-    #plot.margin = margin(0.1, 2.5, 0.1, 0.1, "cm"), #top, right, bottom, left
+    # plot.margin = margin(0.1, 2.5, 0.1, 0.1, "cm"), #top, right, bottom, left
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
@@ -87,7 +87,6 @@ setbp1_med_tpm_vp <- setbp1_exp %>%
     axis.title.y = element_text(face = "bold"),
     axis.title.x = element_text(face = "bold"),
     title = element_text(face = "bold"),
-    #plot.title = element_text(size = 14, hjust = 0.5)
   )
 # add boxplot
 p <- p + geom_boxplot(width = 0.1) + coord_flip()
@@ -101,19 +100,21 @@ ggsave(
 
 # RIDGELINE PLOT - SETBP1 TISSUES
 sb1_tissue_plot <- ggplot(setbp1_med_tpm_vp, aes(x = TPM, y = reorder(Tissue, TPM, FUN = median), fill = Affected)) +
-  ggridges:::stat_density_ridges(quantile_lines = TRUE, quantiles = 2,
-                                 alpha = 0.8) +
+  ggridges:::stat_density_ridges(
+    quantile_lines = TRUE, quantiles = 2,
+    alpha = 0.8
+  ) +
   scale_fill_manual(values = c(
     "TRUE" = alpha("#2C1D6C", 0.75),
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
   geom_text(aes(x = Max, label = nSamples, fontface = "bold"), nudge_x = 1, nudge_y = 0.22) +
-  #scale_y_continuous(limits = c(-0.7, 7)) +
-  #geom_text(aes(y = nSamples, label = Max, vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
+  # scale_y_continuous(limits = c(-0.7, 7)) +
+  # geom_text(aes(y = nSamples, label = Max, vjust = 0.5, fontface = "bold"), nudge_y = 1.2) +
   scale_x_continuous(limits = c(-0.5, 7)) +
   labs(x = "TPM Scaled (1+log2)", y = "GTEx Tissue") +
   theme(
-    #plot.margin = margin(0.1, 2.5, 0.1, 0.1, "cm"), #top, right, bottom, left
+    # plot.margin = margin(0.1, 2.5, 0.1, 0.1, "cm"), #top, right, bottom, left
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
@@ -126,7 +127,6 @@ sb1_tissue_plot <- ggplot(setbp1_med_tpm_vp, aes(x = TPM, y = reorder(Tissue, TP
     axis.title.y = element_text(face = "bold"),
     axis.title.x = element_text(face = "bold"),
     title = element_text(face = "bold"),
-    #plot.title = element_text(size = 14, hjust = 0.5)
   )
 ggsave(
   here(
@@ -143,33 +143,32 @@ brain_meta <- filter(gtex_meta, SMTS == "Brain")
 brain_tpm <- read.csv(here("results/SETBP1_Expression/BRAIN_SETBP1_targets_tpm.csv"), row.names = 1)
 
 # find brain regions
-#brain_regions <- t(brain_tpm) %>% rownames_to_column(., var = "")
-brain_regions <- brain_tpm %>% 
+brain_regions <- brain_tpm %>%
   pivot_longer(., cols = starts_with("GTEX"), names_to = "SAMPID", values_to = "TPM") %>%
   mutate(SAMPID = str_replace(SAMPID, "\\.1$", "")) %>% # remove trailing .1's
-  mutate(SAMPID = str_replace_all(SAMPID, "\\.", "-")) %>%  # sub . for _
-  inner_join(., brain_meta, by = "SAMPID") %>% #combine with gtex metadata
+  mutate(SAMPID = str_replace_all(SAMPID, "\\.", "-")) %>% # sub . for _
+  inner_join(., brain_meta, by = "SAMPID") %>% # combine with gtex metadata
   mutate(Region = str_remove(SMTSD, "Brain - "), .before = TPM)
 
 
 # RIDGELINE PLOT - SETBP1 BRAIN REGIONS
-br_plot <- brain_regions %>% mutate(TPM = log(1 + TPM, base = 2)) %>%
+br_plot <- brain_regions %>%
+  mutate(TPM = log(1 + TPM, base = 2)) %>%
+  mutate(Region = str_trunc(Region, 20)) %>%
   ggplot(., aes(x = TPM, y = reorder(Region, TPM, FUN = median), fill = Tissue)) +
-  #x = reorder(Tissue, Median_TPM, FUN = median),
   ggridges:::stat_density_ridges(quantile_lines = TRUE, quantiles = 2) +
-   scale_fill_manual(values = c(alpha("#2C1D6C", 0.65))) +
-  labs(x = "TPM Scaled (1+log2)", y = "Brain Region") +
+  scale_fill_manual(values = c(alpha("#2C1D6C", 0.65))) +
+  labs(x = "TPM Scaled (1+log2)", y = "") +
   theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
     text = element_text(family = "Helvetica"),
-    axis.text.x = element_text(color = "black", size = 10),
-    axis.text.y = element_text(color = "black"),
+    axis.text.x = element_text(color = "black", size = 25),
+    axis.text.y = element_text(color = "black", size = 25),
     axis.text = element_text(face = "bold"),
     legend.position = "none",
-    axis.title.y = element_text(face = "bold"),
-    axis.title.x = element_text(face = "bold"),
+    axis.title.x = element_text(face = "bold", size = 15),
     title = element_text(face = "bold"),
   )
 ggsave(
@@ -180,6 +179,7 @@ ggsave(
 )
 
 ### SETBP1 TARGET EXPRESSION ACROSS TISSUES
+
 # calculate median tpm of each gene for each tissue and compile list
 tissues_median_tpm <- map_dfc(setbp1_files, get_gtex_median_tpm)
 
@@ -214,23 +214,23 @@ tissues_med_tpm_vp <- tissues_median_tpm %>%
   ) %>% # remove excess from filenames
   mutate(Tissue = str_replace_all(Tissue, " ", "")) %>% # remove whitespace
   mutate(Affected = ifelse(Tissue %in% affected, "TRUE", "FALSE")) %>%
-  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " ")))  %>%
+  mutate(Tissue = str_to_title(str_replace_all(Tissue, "_", " "))) %>%
   group_by(Tissue) %>%
-  mutate(Max = max(Median_TPM, na.rm = TRUE)) 
-  
-  
-  # VIOLIN PLOT - SETBP1 TARGETS ACROSS TISSUES
-  p <- tissues_med_tpm_vp %>% ggplot(aes(
-    x = reorder(Tissue, Median_TPM, FUN = median),
-    y = Median_TPM, color = Affected
-  )) +
+  mutate(Max = max(Median_TPM, na.rm = TRUE))
+
+
+# VIOLIN PLOT - SETBP1 TARGETS ACROSS TISSUES
+p <- tissues_med_tpm_vp %>% ggplot(aes(
+  x = reorder(Tissue, Median_TPM, FUN = median),
+  y = Median_TPM, color = Affected
+)) +
   geom_violin(trim = FALSE) +
   scale_color_manual(values = c(
     "TRUE" = alpha("#2C1D6C", 0.75),
     "FALSE" = alpha("#D3B1C5", 0.5)
   )) +
   ylab("Median TPM Scaled (1+log2)") +
-  #ggtitle("Expression of SETBP1 Targets Across GTEx Tissues") +
+  # ggtitle("Expression of SETBP1 Targets Across GTEx Tissues") +
   labs(x = "GTEx Tissue") +
   theme(
     panel.grid.major = element_blank(),
@@ -245,7 +245,6 @@ tissues_med_tpm_vp <- tissues_median_tpm %>%
     axis.title.y = element_text(face = "bold"),
     axis.title.x = element_text(face = "bold"),
     title = element_text(face = "bold"),
-    #plot.title = element_text(size = 14, hjust = 0.5)
   )
 # add boxplot
 p <- p + geom_boxplot(width = 0.1) + coord_flip()
