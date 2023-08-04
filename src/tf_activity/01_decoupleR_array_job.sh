6846225fc8234b780bc13d8379d4b25669d30c75
jordanwhitlock <jwhitlock@uab.edu>
2023-08-04 13:10:39 -0500

    renaming

--- src/tf_activity/01_decoupleR_array_job.sh
+++ src/tf_activity/01_decoupleR_array_job.sh
@@ -4,14 +4,14 @@
 
 #SBATCH --job-name=gtex_decoupleR 
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=jbarham3@uab.edu #CHANGE THE EMAIL TO YOUR OWN
+#SBATCH --mail-user=$USER@uab.edu #CHANGE THE EMAIL TO YOUR OWN
 #
 #Number of tasks needed for this job, the partition, and parameters
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=255000
 #SBATCH --nodes=1
 #SBATCH --time=50:00:00 #Note: '50:00:00 
-#SBATCH --partition=largemem #Note: when running brain, can use largemem. Partition info here: https://docs.rc.uab.edu/cheaha/hardware/#partitions
+#SBATCH --partition=largemem 
 #
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
@@ -23,11 +23,11 @@
 module load R
 module load Singularity/3.5.2-GCC-5.4.0-2.26
 
-wd=/data/user/jbarham3/230323_JW_DiseaseNetworks #change this to match your project directory path
+wd="$USER_DATA/230323_JW_DiseaseNetworks" #filepath to repo
+src="$USER_DATA/230323_JW_DiseaseNetworks/src" #be sure that your subdirectories are structured the same
 
 export SINGULARITYENV_PASSWORD='pass'
-export SINGULARITYENV_USER='jbarham3' #change this to your user
-
+export SINGULARITYENV_USER='$USER'
 
 cd ${wd}
 
@@ -38,10 +38,16 @@ GTEX_FILE=`echo ${SAMPLE_ARRAY[$SLURM_ARRAY_TASK_ID]}` #extracts a single input 
 
 
 #input prior network file
-PRIOR_NET=${wd}/data/human_prior_tri.csv
+PRIOR_NET="/data/project/lasseigne_lab/JordanWhitlock/230323_JW_DiseaseNetworks/data/human_prior_tri.csv"
 echo "Prior Network from ${PRIOR_NET}"
 
 #set minimum interaction number
 MIN_N=5
 
-singularity exec --cleanenv --no-home -B ${wd} -B /data/project/lasseigne_lab/DATASET_dir/TCGA_GTEx_CCLE_HPA_220807/gtex/ $USER_DATA/230323_JW_DiseaseNetworks/bin/PANDA_construction_docker/jw_diseasenetworks_1.0.0.sif Rscript --vanilla ${wd}/src/tf_activity/01_decoupleR_analysis.R ${GTEX_FILE} ${PRIOR_NET} ${MIN_N}
+# NOTE user must have already pulled from docker and built .sif file with singularity below (jordanwhitlock/setbp1_manuscript_panda_1.0.1)
+#singularity exec --cleanenv --no-home -B ${wd} -B /data/project/lasseigne_lab/DATASET_dir/TCGA_GTEx_CCLE_HPA_220807/gtex/ ${wd}/bin/PANDA_construction_docker/jw_diseasenetworks_1.0.0.sif Rscript --vanilla ${wd}/src/tf_activity/01_decoupleR_analysis.R ${GTEX_FILE} ${PRIOR_NET} ${MIN_N} ${wd}
+singularity exec --cleanenv --containall \
+  -B ${wd} \
+  -B /data/project/lasseigne_lab/ \
+  ${wd}/bin/PANDA_construction_docker/jw_diseasenetworks_1.0.0.sif \
+  Rscript --vanilla ${wd}/src/tf_activity/02_decoupleR_analysis.R ${GTEX_FILE} ${PRIOR_NET} ${MIN_N} ${wd}
